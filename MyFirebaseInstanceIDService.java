4791ca24c238177d4e37ddd27d01e925acf5a0ba
Indian <alpeshhirania@gmail.com>
2019-01-08 14:55:36 +0530

    Add files via upload

---
+++ MyFirebaseInstanceIDService.java
@@ -0,0 +1,50 @@
+package com.oozeetech.padkranti.fcm;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import java.io.IOException;
+
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebaseIIDService";
+
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+
+    // [START refresh_token]
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = null;
+        try {
+            refreshedToken = FirebaseInstanceId.getInstance().getToken("","");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        Logcat.e(TAG, "Refreshed token: " + refreshedToken);
+//        PreferenceData.setStringPref(PreferenceData.KEY_GCM_ID,this,refreshedToken);
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(refreshedToken);
+    }
+    // [END refresh_token]
+
+    /**
+     * Persist token to third-party servers.
+     *
+     * Modify this method to associate the user's FCM InstanceID token with any server-side account
+     * maintained by your application.
+     *
+     * @param token The new token.
+     */
+    private void sendRegistrationToServer(String token) {
+        // TODO: Implement this method to send token to your app server.
+    }
+}
+
